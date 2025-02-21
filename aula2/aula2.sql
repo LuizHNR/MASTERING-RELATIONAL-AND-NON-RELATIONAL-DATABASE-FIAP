a6df891ded5c552742f57277a68ed312a5c0f649
LuizHNR <luizhneri12@gmail.com>
2025-02-21 09:04:34 -0300

    to, DATE

--- aula2/aula2.sql
+++ aula2/aula2.sql
@@ -1,22 +1,26 @@
-Aula2 21/02/2025
+-- Aula2 21/02/2025
 
 SET SERVEROUTPUT ON
 
 Declare
 
     v_nome varchar2(30) := 'Presunto';
-    v_sal number(7,2) := 1000;
+    v_sal number(7,2) := 1000.72;
+    v_hoje DATE := SYSDATE;
     
 BEGIN
 
-    DBMS_OUTPUT.PUT_LINE(v_nome || ' recebe ' || to_char(v_sal,'9999.00'));
+--  L é o $ quando printar vai mostrar o $*****,**
+    DBMS_OUTPUT.PUT_LINE(v_nome || ' recebe ' || to_char(v_sal,'L9999.00') || v_hoje);
     
 --  Jeito errado  
 --  select ename from emp where sal > 3000
 
     select ename, sal into v_name, v_sal from emp where sal > 3000;
+
+    v_hoje := v_hoje +1;
     
-    DBMS_OUTPUT.PUT_LINE(v_nome);
+    DBMS_OUTPUT.PUT_LINE(v_nome || v_sal || v_hoje);
     
 END;
 
