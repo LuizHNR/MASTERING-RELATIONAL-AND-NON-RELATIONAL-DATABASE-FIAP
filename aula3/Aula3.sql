5177fe1b1387dc069eef9550f5ebcfa2138f7bdf
LuizHNR <luizhneri12@gmail.com>
2025-02-28 09:50:16 -0300

    atualização

--- aula3/Aula3.sql
+++ aula3/Aula3.sql
@@ -14,15 +14,25 @@ DECLARE
 
 BEGIN
 
+/*
+    Length para saber o tamanho de algo
     v_valor:= LENGTH(v_nome);
+    
     DBMS_OUTPUT.PUT_LINE(v_nome || ' tem ' || v_valor || ' letras');
     DBMS_OUTPUT.PUT_LINE('Você nasceu dia ' || to_char(v_niver, 'DD'));
     DBMS_OUTPUT.PUT_LINE('Era uma: ' || to_char(v_niver, 'day'));
     DBMS_OUTPUT.PUT_LINE(v_niver);
     
--- MONTHS_BETWEEN valor entre meses 
--- Round é arredondar
+    MONTHS_BETWEEN valor entre meses e Round é arredondar
     v_valor := Round(MONTHS_BETWEEN(SYSDATE, v_niver));
     DBMS_OUTPUT.PUT_LINE('Você viveu: ' || v_valor);
+*/
+    select ename, sal INTO v_nome, v_valor from emp where sal > 3000;
+    
+    DECLARE
+        v_nome VARCHAR2(30) := 'Maria';
+    BEGIN
+        v_valor := v_valor + 1;
+    END;
     
 END;
